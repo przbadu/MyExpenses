6c0c67fcfc6ae22f30e801ce08b316a431f37fee
przbadu <przbadu.social@gmail.com>
2021-08-29 08:59:54 +0545

    test paper and material icons are working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {Provider as PaperProvider, Button} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const App = () => {
   return (
     <PaperProvider>
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <Icon name="home" color="tomato" style={{fontSize: 32}} />
-        <Text>Home Page Screen</Text>
+        <Button icon="home">Home Screen</Button>
       </View>
     </PaperProvider>
   );
