ee504e40a4a1988521a83c941f756417378024d3
przbadu <przbadu.social@gmail.com>
2021-10-15 09:55:31 +0545

    Filter was not applied to charts, fixed

--- src/screens/Home/AppLineChart.tsx
+++ src/screens/Home/AppLineChart.tsx
@@ -1,12 +1,9 @@
-import withObservables from '@nozbe/with-observables';
-import {useFocusEffect, useNavigation} from '@react-navigation/core';
-import dayjs from 'dayjs';
 import React from 'react';
-import {Dimensions, FlatList, ScrollView, View} from 'react-native';
-import {BarChart, LineChart} from 'react-native-chart-kit';
-import {Caption, Subheading, Surface, useTheme} from 'react-native-paper';
+import {Dimensions, View} from 'react-native';
+import {LineChart} from 'react-native-chart-kit';
+import {ActivityIndicator, useTheme} from 'react-native-paper';
 import Svg, {Rect, Text as TextSVG} from 'react-native-svg';
-import {CategoryRow, SummaryCard} from '../../components';
+import {lineChartFilterProps} from '../../database/helpers';
 import {
   amountSeperator,
   chartConfig,
@@ -17,75 +14,27 @@ import {
   responsiveHeight,
   responsiveWidth,
 } from '../../lib';
-import {
-  categoryWithTransactionInfo,
-  lineChartData,
-  lineChartFilterProps,
-  observeTransactions,
-  transactionTypeSummary,
-} from '../../database/helpers';
-import {Transaction, TransactionTypeEnum} from '../../database/models';
 import {CurrencyContext} from '../../store/context';
 
-const _AppLineChart = ({
-  transactions,
+const AppLineChart = ({
+  incomeChartData,
+  expenseChartData,
   filter,
+  loading,
 }: {
-  transactions: Transaction[];
   filter: lineChartFilterProps;
+  incomeChartData: {amount: number; date: string}[];
+  expenseChartData: {amount: number; date: string}[];
+  loading: boolean;
 }) => {
   const {currency} = React.useContext(CurrencyContext);
-  const [balance, setBalance] = React.useState(0);
-  const [categories, setCategories] = React.useState<[]>([]);
-  const [incomeChartData, setIncomeChartData] = React.useState<
-    {amount: number; date: string}[]
-  >([]);
-  const [expenseChartData, setExpenseChartData] = React.useState<
-    {amount: number; date: string}[]
-  >([]);
-  const {colors, dark} = useTheme();
+  const {colors} = useTheme();
   const [tooltipPos, setTooltipPos] = React.useState({
     x: 0,
     y: 0,
     visible: false,
     value: 0,
   });
-  const navigation = useNavigation();
-
-  useFocusEffect(
-    React.useCallback(() => {
-      fetchCategories();
-      fetchChartData();
-      fetchSummary();
-    }, []),
-  );
-
-  const fetchChartData = async () => {
-    const incomeData = await lineChartData(filter, 'Income');
-    const expenseData = await lineChartData(filter, 'Expense');
-    setIncomeChartData(incomeData);
-    setExpenseChartData(expenseData);
-  };
-
-  const fetchSummary = async () => {
-    const summary: {
-      transaction_type: TransactionTypeEnum;
-      sum_amount: number;
-    }[] = await transactionTypeSummary();
-    const _balance = summary.reduce(
-      (sum, tran) =>
-        tran.transaction_type == TransactionTypeEnum.expense
-          ? sum - tran.sum_amount
-          : sum + tran.sum_amount,
-      0,
-    );
-    setBalance(_balance || 0);
-  };
-
-  const fetchCategories = async () => {
-    const _categories = await categoryWithTransactionInfo();
-    setCategories(_categories);
-  };
 
   function renderTooltip(symbol: string) {
     const label = numberToCurrency(tooltipPos.value || 0, currency) + symbol;
@@ -115,143 +64,90 @@ const _AppLineChart = ({
     );
   }
 
-  function renderLineChart() {
-    const maxNumber = Math.max.apply(
-      Math,
-      incomeChartData.map(d => d.amount),
-    );
-    const siSymbol = getSiSymbol(maxNumber);
-    const incomeData: number[] = incomeChartData.map(
-      (d: {amount: number}): number => amountSeperator(+d.amount, maxNumber),
-    );
-    const expenseData: number[] = expenseChartData.map(
-      (d: {amount: number}): number => amountSeperator(+d.amount, maxNumber),
-    );
+  const maxNumber = Math.max.apply(
+    Math,
+    incomeChartData.map(d => d.amount),
+  );
+  const siSymbol = getSiSymbol(maxNumber);
+  const incomeData: number[] = incomeChartData.map(
+    (d: {amount: number}): number => amountSeperator(+d.amount, maxNumber),
+  );
+  const expenseData: number[] = expenseChartData.map(
+    (d: {amount: number}): number => amountSeperator(+d.amount, maxNumber),
+  );
 
-    let labels;
-    if (incomeChartData.length) {
-      labels = incomeChartData.map((d: {date: string}) => {
-        if (filter === 'yearly') return numToMonthName(d.date);
-        else if (filter === 'monthly') return d.date;
-        else if (filter === 'weekly') return numToWeekName(d.date);
-        else return d.date;
-      });
-    } else {
-      labels = expenseChartData.map((d: {date: string}) => {
-        if (filter === 'yearly') return numToMonthName(d.date);
-        else if (filter === 'monthly') return d.date;
-        else if (filter === 'weekly') return numToWeekName(d.date);
-        else return d.date;
-      });
-    }
-
-    let lineChartDataset = [];
-    let legends = [];
-    if (incomeData.length) {
-      legends.push('Income');
-      lineChartDataset.push({
-        data: incomeData,
-        color: () => colors.success,
-      });
-    }
-    if (expenseData.length) {
-      legends.push('Expense');
-      lineChartDataset.push({
-        data: expenseData,
-        color: () => colors.notification,
-      });
-    }
-
-    return (
-      <LineChart
-        data={{
-          labels: labels,
-          datasets: lineChartDataset,
-          legend: legends,
-        }}
-        width={Dimensions.get('window').width - responsiveWidth(5)} // from react-native
-        height={responsiveHeight(30)}
-        // yAxisLabel="$"
-        yAxisSuffix={siSymbol}
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={chartConfig(colors)}
-        decorator={() => (tooltipPos.visible ? renderTooltip(siSymbol) : null)}
-        onDataPointClick={data => {
-          let isSamePoint = tooltipPos.x === data.x && tooltipPos.y === data.y;
-
-          isSamePoint
-            ? setTooltipPos(previousState => {
-                return {
-                  ...previousState,
-                  value: data.value,
-                  visible: !previousState.visible,
-                };
-              })
-            : setTooltipPos({
-                x: data.x,
-                value: data.value,
-                y: data.y,
-                visible: true,
-              });
-        }}
-        withInnerLines={false}
-        withHorizontalLines={false}
-        withVerticalLines={false}
-        bezier
-      />
-    );
-  }
-
-  if (incomeChartData.length || expenseChartData.length) {
-    return (
-      <FlatList
-        nestedScrollEnabled
-        data={categories}
-        keyExtractor={item => `category-list-${item.id}`}
-        ListHeaderComponent={
-          <>
-            <SummaryCard
-              balance={balance}
-              containerStyles={{marginHorizontal: 10, marginTop: 10}}
-              showIncomeExpense={false}
-            />
-            <Surface style={{marginHorizontal: 10}}>
-              {renderLineChart()}
-            </Surface>
-
-            {/* render categories */}
-            <Subheading style={{marginHorizontal: 10, marginTop: 20}}>
-              {`Categories`.toUpperCase()}
-            </Subheading>
-          </>
-        }
-        renderItem={({item}) => (
-          <View style={{marginHorizontal: 10}}>
-            <CategoryRow
-              category={item}
-              onPress={() =>
-                navigation.navigate('CategoryTransactions', {
-                  categoryId: item.id,
-                  categoryName: item.name,
-                })
-              }
-            />
-          </View>
-        )}
-        style={{marginBottom: 80}}
-      />
-    );
+  let labels;
+  if (incomeChartData.length) {
+    labels = incomeChartData.map((d: {date: string}) => {
+      if (filter === 'yearly') return numToMonthName(d.date);
+      else if (filter === 'monthly') return d.date;
+      else if (filter === 'weekly') return numToWeekName(d.date);
+      else return d.date;
+    });
   } else {
-    return (
-      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-        <Caption>Add transactions to generate report</Caption>
-      </View>
-    );
+    labels = expenseChartData.map((d: {date: string}) => {
+      if (filter === 'yearly') return numToMonthName(d.date);
+      else if (filter === 'monthly') return d.date;
+      else if (filter === 'weekly') return numToWeekName(d.date);
+      else return d.date;
+    });
   }
-};
 
-const AppLineChart = withObservables([], () => ({
-  transactions: observeTransactions(),
-}))(_AppLineChart);
+  let lineChartDataset = [];
+  let legends = [];
+  if (incomeData.length) {
+    legends.push('Income');
+    lineChartDataset.push({
+      data: incomeData,
+      color: () => colors.success,
+    });
+  }
+  if (expenseData.length) {
+    legends.push('Expense');
+    lineChartDataset.push({
+      data: expenseData,
+      color: () => colors.notification,
+    });
+  }
+
+  return (
+    <LineChart
+      data={{
+        labels: labels,
+        datasets: lineChartDataset,
+        legend: legends,
+      }}
+      width={Dimensions.get('window').width - responsiveWidth(5)} // from react-native
+      height={responsiveHeight(30)}
+      // yAxisLabel="$"
+      yAxisSuffix={siSymbol}
+      yAxisInterval={1} // optional, defaults to 1
+      chartConfig={chartConfig(colors)}
+      decorator={() => (tooltipPos.visible ? renderTooltip(siSymbol) : null)}
+      onDataPointClick={data => {
+        let isSamePoint = tooltipPos.x === data.x && tooltipPos.y === data.y;
+
+        isSamePoint
+          ? setTooltipPos(previousState => {
+              return {
+                ...previousState,
+                value: data.value,
+                visible: !previousState.visible,
+              };
+            })
+          : setTooltipPos({
+              x: data.x,
+              value: data.value,
+              y: data.y,
+              visible: true,
+            });
+      }}
+      withInnerLines={false}
+      withHorizontalLines={false}
+      withVerticalLines={false}
+      bezier
+    />
+  );
+};
 
 export {AppLineChart};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,84 @@
+import withObservables from '@nozbe/with-observables';
+import {useFocusEffect, useNavigation} from '@react-navigation/core';
 import React from 'react';
-import {ScrollView, View} from 'react-native';
-import {Appbar, Card, IconButton, Surface} from 'react-native-paper';
-import {AppChip} from '../../components';
-import {lineChartFilterProps} from '../../database/helpers';
+import {FlatList, ScrollView, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Appbar,
+  Caption,
+  Subheading,
+  Surface,
+} from 'react-native-paper';
+import {AppChip, CategoryRow, SummaryCard} from '../../components';
+import {
+  categoryWithTransactionInfo,
+  lineChartData,
+  lineChartFilterProps,
+  observeTransactions,
+  transactionTypeSummary,
+} from '../../database/helpers';
+import {Transaction, TransactionTypeEnum} from '../../database/models';
 import {AppLineChart} from './AppLineChart';
-import {AppPieChart} from './AppPieChart';
 
-const Home = () => {
-  const [chartStyle, setChartStyle] = React.useState<'line' | 'pie'>('line');
+let Home = ({transactions}: {transactions: Transaction[]}) => {
   const [filter, setFilter] = React.useState<lineChartFilterProps>('yearly');
+  const [balance, setBalance] = React.useState(0);
+  const [categories, setCategories] = React.useState<[]>([]);
+  const [incomeChartData, setIncomeChartData] = React.useState<
+    {amount: number; date: string}[]
+  >([]);
+  const [expenseChartData, setExpenseChartData] = React.useState<
+    {amount: number; date: string}[]
+  >([]);
+  const [loading, setLoading] = React.useState(false);
+  const navigation = useNavigation();
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchReports();
+    }, []),
+  );
+
+  React.useEffect(() => {
+    fetchChartData();
+  }, [filter]);
+
+  async function fetchReports() {
+    setLoading(true);
+    await fetchCategories();
+    await fetchChartData();
+    await fetchSummary();
+    setLoading(false);
+  }
+
+  const fetchChartData = async () => {
+    setLoading(true);
+    const incomeData = await lineChartData(filter, 'Income');
+    const expenseData = await lineChartData(filter, 'Expense');
+    setIncomeChartData(incomeData);
+    setExpenseChartData(expenseData);
+    setLoading(false);
+  };
+
+  const fetchSummary = async () => {
+    const summary: {
+      transaction_type: TransactionTypeEnum;
+      sum_amount: number;
+    }[] = await transactionTypeSummary();
+    const _balance = summary.reduce(
+      (sum, tran) =>
+        tran.transaction_type == TransactionTypeEnum.expense
+          ? sum - tran.sum_amount
+          : sum + tran.sum_amount,
+      0,
+    );
+    setBalance(_balance || 0);
+  };
+
+  const fetchCategories = async () => {
+    const _categories = await categoryWithTransactionInfo();
+    setCategories(_categories);
+  };
 
   function renderFilters() {
     return (
@@ -24,7 +94,7 @@ const Home = () => {
             justifyContent: 'space-between',
             alignItems: 'center',
           }}>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'row', marginBottom: 5}}>
             <AppChip
               selected={filter === 'weekly'}
               onPress={() => setFilter('weekly')}>
@@ -41,19 +111,75 @@ const Home = () => {
               Yearly
             </AppChip>
           </View>
-
-          <View style={{flexDirection: 'row'}}>
-            <IconButton
-              icon="chart-timeline-variant"
-              onPress={() => setChartStyle('line')}
-            />
-            <IconButton
-              icon="format-list-bulleted"
-              onPress={() => setChartStyle('pie')}
-            />
-          </View>
         </ScrollView>
       </Surface>
+    );
+  }
+
+  function renderListHeaderComponent() {
+    return (
+      <>
+        <SummaryCard
+          balance={balance}
+          containerStyles={{marginHorizontal: 10, marginTop: 10}}
+          showIncomeExpense={false}
+        />
+        <Surface style={{marginHorizontal: 10}}>
+          {loading ? (
+            <ActivityIndicator animating style={{marginBottom: 10}} />
+          ) : (
+            <AppLineChart
+              expenseChartData={expenseChartData}
+              incomeChartData={incomeChartData}
+              filter={filter}
+              loading={loading}
+            />
+          )}
+        </Surface>
+
+        {/* render categories */}
+        <Subheading style={{marginHorizontal: 10, marginTop: 20}}>
+          {`Categories`.toUpperCase()}
+        </Subheading>
+      </>
+    );
+  }
+
+  function renderItem(item: any) {
+    return (
+      <View style={{marginHorizontal: 10}}>
+        <CategoryRow
+          category={item}
+          onPress={() =>
+            navigation.navigate('CategoryTransactions', {
+              categoryId: item.id,
+              categoryName: item.name,
+            })
+          }
+        />
+      </View>
+    );
+  }
+
+  function renderEmptyPlaceholder() {
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Caption>Add transactions to generate report</Caption>
+      </View>
+    );
+  }
+
+  function renderCharts() {
+    return (
+      <FlatList
+        nestedScrollEnabled
+        data={categories}
+        keyExtractor={item => `category-list-${item.id}`}
+        ListHeaderComponent={renderListHeaderComponent()}
+        ListEmptyComponent={renderEmptyPlaceholder}
+        renderItem={({item}) => renderItem(item)}
+        style={{marginBottom: 80}}
+      />
     );
   }
 
@@ -65,14 +191,13 @@ const Home = () => {
       </Appbar.Header>
 
       {renderFilters()}
-
-      {chartStyle === 'line' ? (
-        <AppLineChart filter={filter} />
-      ) : (
-        <AppPieChart />
-      )}
+      {renderCharts()}
     </>
   );
 };
+
+Home = withObservables([], () => ({
+  transactions: observeTransactions(),
+}))(Home);
 
 export {Home};
