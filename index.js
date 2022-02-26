98867f50bf406ca2b2ec749d1d6404e355a76971
przbadu <pushparaj.badu@gmail.com>
2022-02-26 09:42:44 +0545

    refactor(#33): upgrade React Native and other libraries, setup testcase
    Upgraded React native and other node modules, setup testcase for the project

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
  * @format
  */
 
-import * as Sentry from '@sentry/react-native';
 import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
-import App from './src/App';
-import {SentryConfig} from './src/lib';
-
-Sentry.init(SentryConfig);
 
 AppRegistry.registerComponent(appName, () => App);
